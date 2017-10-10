3546689848399133f8593cf9a921e36a26eea256
ECravener <EthanCravener@case.edu>
2017-10-09 20:21:49 -0700

    Added some commented out IPS code

---
+++ Lumicast/Lumicast-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  Lumicast-Bridging-Header.h
+//  Lumicast
+//
+//  Created by Ethan Cravener on 10/9/17.
+//  Copyright © 2017 Liz Trinnes. All rights reserved.
+//
+
+#ifndef Lumicast_Bridging_Header_h
+#define Lumicast_Bridging_Header_h
+
+#import "Angle.h"
+#import "LumicastErrors.h"
+#import "LumicastSdk.h"
+#import "Position.h"
+
+#endif /* Lumicast_Bridging_Header_h */
--- Lumicast/Lumicast/AppDelegate.swift
+++ Lumicast/Lumicast/AppDelegate.swift
@@ -21,8 +21,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        FIRApp.configure()
+        //var mLumicastSDK: LumicastSdk
+        //mLumicastSDK.delegate = self;
+        //mLumicastSDK.initialize;
+               
         
+        
+        FIRApp.configure();
         
         return true
     }
